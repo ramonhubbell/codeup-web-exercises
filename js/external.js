182f232dd39a96c228e326f612774e48c8b384fd
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-03-17 14:29:03 -0500

    external javascript exercise

---
+++ js/external.js
@@ -0,0 +1,44 @@
+// console.log("Hello from external JavaScript.");
+
+// alert("Welcome to my Website.");
+
+// var userInput = prompt('What is your favorite color?');
+// alert(userInput + " is also my favorite color!");
+
+var userInputLittleMermaid = prompt("How many days was LittleMermaid rented for?");
+var userInputBrotherBear = prompt("How many days was Brother Bear rented for?");
+var userInputHercules = prompt("How many days was Hercules rented for?");
+totalRental = ((userInputLittleMermaid * 1) + (userInputBrotherBear * 1) + (userInputHercules * 1)) * 3;
+alert("You will have to pay $" + totalRental + " for renting Little Mermaid, Brother Bear, and Hercules.");
+// var littleMermaid = 3, brotherBear = 5, hercules = 1;
+// (littleMermaid + brotherBear + hercules) * 3;
+
+// var userInputGoogle = prompt("How much will Google pay you per hour?");
+// var userInputGoogle2 = prompt("How many hours did you work for Google this week?")
+// var userInputAmazon = prompt("How much will Amazon pay you per hour?");
+// var userInputAmazon2 = prompt("How many hours did you work for Amazon this week?")
+// var userInputFacebook = prompt("How much will Facebook pay you per hour?");
+// var userInputFacebook2 = prompt("How many hours did you work for Facebook this week?");
+// totalPayment = (userInputGoogle * userInputGoogle2) + (userInputAmazon * userInputAmazon2) + (userInputFacebook * userInputFacebook2);
+// alert("You will receive $" + totalPayment + " this week.");
+
+
+// var googleRate = 400, amazonRate = 380, facebook = 350;
+// (facebook * 10) + (googleRate * 6) + (amazonRate * 4);
+
+// var classIsFull = prompt("Is the class full?");
+// var ConflictsWithSchedule = prompt("Does the class conflict with your schedule?");
+// alert("You can enroll in class!");
+
+// var PremiumMember = prompt("Are you a premium member?");
+// var OfferExpired = prompt("Has the offer expired?");
+// var numberOfItems = prompt("Have you bought more than two items?")
+// alert("The offer will be applied!");
+// // var numberOfItemsBought = 2;
+// var hasOfferExpired = false;
+// var isPremiumMember = true;
+// var isOfferValid = isPremiumMember || (numberOfItemsBought >= 2 && !!hasOfferExpired);
+
+
+
+
