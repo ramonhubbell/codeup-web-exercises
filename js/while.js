6634c4d21c0323871e54471699a2f09e2ca719f2
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-03-24 13:49:34 -0500

    first exerercise for loops exercise done

---
+++ js/while.js
@@ -0,0 +1,15 @@
+// while (/*condition*/) {
+    // body
+// }
+var i = 2;
+
+while (i <= 65536) {
+    console.log("While loop numbers are: " + i);
+    i = i * 2;
+}
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+Math.floor(Math.random() * 5) + 1;
+
