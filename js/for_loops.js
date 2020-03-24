2584e46c6552c80e6cd45d6ba0640d22f7cb4608
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-03-24 15:06:28 -0500

    completed exercise five for for-loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -22,11 +22,19 @@
 // This expression will generate a random number between 1 and 5
 // Math.floor(Math.random() * 200) + 20;
 
-for (var i = 1; i <= 10; i++) {
-    var randomNumber = Math.floor(Math.random() * 180) + 20;
-    if (randomNumber % 2 == 0){
-        console.log(randomNumber + " is even.");
-    } else {
-        console.log(randomNumber + " is odd.");
-}
+// for (var i = 1; i <= 10; i++) {
+//     var randomNumber = Math.floor(Math.random() * 180) + 20;
+//     if (randomNumber % 2 == 0){
+//         console.log(randomNumber + " is even.");
+//     } else {
+//         console.log(randomNumber + " is odd.");
+// }
+// }
+
+// for (var i = 9; i <= 9; i++) {
+//     console.log(i);
+//     }
+
+for (var i = 100; i >= 5; i -= 5) {
+    console.log("For loop counting down by 5: " + i);
 }
