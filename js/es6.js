59db9663bb84dc5eb2a1e405e3250eeff8fda2a6
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-04-21 12:01:29 -0500

    replaced with arrow functions

--- js/es6.js
+++ js/es6.js
@@ -61,9 +61,13 @@ var names = [];
 users.forEach(function(user) {
   return emails.push(user.email);
 });
+// Rewrote to arrow function.
+users.forEach((user) => emails.push(user.email));
 users.forEach(function(user) {
   return names.push(user.name);
 });
+// Rewrote to arrow function.
+users.forEach((user) => names.push(user.name));
 
 // TODO: replace `var` with `let` in the following declaration
 var developers = [];
