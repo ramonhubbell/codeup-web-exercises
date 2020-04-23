a40e1d8908438b6c89e164f3c99f27608a1326a4
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-04-23 11:16:31 -0500

    finished first exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -37,3 +37,19 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+// Exercise 2: Use .filter to create an array of user objects where each
+// user object has at least 3 languages in the languages array.
+
+var atLeast3Languages = users.filter(function(name){
+    return name.languages.length <= 3;
+});
+
+console.log('atLeast3Languages ' , atLeast3Languages);
+
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// var evens = numbers.filter(function(n) {
+//     return n % 2 === 0;
+// });
+// console.log(evens); // [2, 4, 6, 8, 10]
