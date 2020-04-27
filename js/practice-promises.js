69524f7462ed71db5ab17913501444c5e112632e
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-04-27 12:39:42 -0500

    finished promises exercises

--- js/practice-promises.js
+++ js/practice-promises.js
@@ -40,22 +40,3 @@ console.log(wait()); // a pending promise
 
 wait(1000).then((ms) => console.log(`You\'ll see this after ${ms/1000} seconds`));
 wait(3000).then((ms) => console.log(`You\'ll see this after ${ms/1000} seconds`));
-
-// function makeRequest() {
-// //     return new Promise((resolve, reject) => {
-// //         setTimeout(() => {
-// //             if (Math.random() > 0.1) {
-// //                 resolve('Here is your data: ...');
-// //             } else {
-// //                 reject('Network Connection Error!');
-// //             }
-// //         }, 1500);
-// //     });
-// // }
-// //
-// // const request = makeRequest();
-// // console.log(request); // pending promise
-// // request.then(message => console.log('Promise resolved!', message));
-// // // if resolved, will log "Promise resolved!" and "Here is your data: ..."
-// // request.catch(message => console.log('Promise rejected!', message));
-// // // if rejected, will log "Promise rejected!" and "Network Connection Error!"
