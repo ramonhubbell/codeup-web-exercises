d969284973cb78acd02f5fc30da87f5d7201f38a
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-04-27 12:37:05 -0500

    finished promises exercises

--- js/practice-promises.js
+++ js/practice-promises.js
@@ -23,4 +23,39 @@ function lastCommit (username) {
 }
 
 lastCommit('ramonhubbell')
-    .then(lastCommitDate => console.log('lastCommitDate ', lastCommitDate));
+    // .then(lastCommitDate => console.log('lastCommitDate ', lastCommitDate));
+    .then(lastCommitDate => document.body.innerHTML = `lastCommitDate: ${lastCommitDate}`);
+
+// Write a function named wait that accepts a number as a parameter, and returns
+// a promise that resolves after the passed number of milliseconds.
+
+function wait (milliseconds) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve ( milliseconds) ;
+        }, milliseconds);
+    });
+}
+console.log(wait()); // a pending promise
+
+wait(1000).then((ms) => console.log(`You\'ll see this after ${ms/1000} seconds`));
+wait(3000).then((ms) => console.log(`You\'ll see this after ${ms/1000} seconds`));
+
+// function makeRequest() {
+// //     return new Promise((resolve, reject) => {
+// //         setTimeout(() => {
+// //             if (Math.random() > 0.1) {
+// //                 resolve('Here is your data: ...');
+// //             } else {
+// //                 reject('Network Connection Error!');
+// //             }
+// //         }, 1500);
+// //     });
+// // }
+// //
+// // const request = makeRequest();
+// // console.log(request); // pending promise
+// // request.then(message => console.log('Promise resolved!', message));
+// // // if resolved, will log "Promise resolved!" and "Here is your data: ..."
+// // request.catch(message => console.log('Promise rejected!', message));
+// // // if rejected, will log "Promise rejected!" and "Network Connection Error!"
